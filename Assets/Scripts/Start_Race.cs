82208acf7f5ecb528598468a59cf549522086064
Yamid-E <127436285+Yamid-E@users.noreply.github.com>
2026-03-26 17:57:36 -0500

    Music_FBX

--- Assets/Scripts/Start_Race.cs
+++ Assets/Scripts/Start_Race.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // Librería vital para cargar niveles
+using UnityEngine.SceneManagement;
+using System.Collections; // Vital para usar temporizadores (Corrutinas)
 
-public class MenuManager : MonoBehaviour
+public class Start_Race : MonoBehaviour
 {
-    public void Start_Race()
+    public AudioSource sfxArranque; 
+
+    public void IniciarCarrera()
     {
+        StartCoroutine(SecuenciaArranque());
+    }
+
+    private IEnumerator SecuenciaArranque()
+    {
+        if (sfxArranque != null)
+        {
+            sfxArranque.Play();
+        }
+
+        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene("Alex Scene"); 
     }
 }
