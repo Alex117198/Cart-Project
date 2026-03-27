c8bb0590cee5208d958ddb654f3bc17875be1ec2
Yamid-E <127436285+Yamid-E@users.noreply.github.com>
2026-03-26 21:42:38 -0500

    Tutorial_UI

--- Assets/Scripts/Start_Race.cs
+++ Assets/Scripts/Start_Race.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections; // Vital para usar temporizadores (Corrutinas)
+using System.Collections;
 
 public class Start_Race : MonoBehaviour
 {
+    public GameObject panelMainMenu;
+    public GameObject panelTutorial;
+
     public AudioSource sfxArranque; 
+
+    public void MostrarTutorial()
+    {
+        panelMainMenu.SetActive(false); // Apagamos el menú
+        panelTutorial.SetActive(true);  // Encendemos el tutorial
+    }
+
 
     public void IniciarCarrera()
     {
@@ -17,8 +27,8 @@ public class Start_Race : MonoBehaviour
         {
             sfxArranque.Play();
         }
-
-        yield return new WaitForSeconds(3.0f);
+        
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene("Pablo Scene"); 
     }
 }
